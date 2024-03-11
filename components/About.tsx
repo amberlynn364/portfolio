b17f417492afb73fc389db76a0545730b55d6e8b
amberlynn <amberlynn364@gmail.com>
2024-03-12 01:56:30 +0300

    feat: add About section

---
+++ components/About.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import SectionHeading from './SectionHeading';
+
+export default function About() {
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        Having received my education in
+        {' '}
+        <span className="font-medium">environmental engineering</span>
+        , decided to continue my education in the field of programming.
+        I started learning programming from technical books, then took coding classes and learned
+        {' '}
+        <span className="font-medium">front-end development</span>
+        .
+        {' '}
+        <span className="italic">My favorite part of programming</span>
+        {' '}
+        is enjoying the results and writing concise code. I
+        {' '}
+        <span className="underline">love</span>
+        {' '}
+        the
+        feeling of having found a solution to a problem. My core stack
+        is
+        {' '}
+        <span className="font-medium">
+          React, Next.js, Node.js, and MongoDB
+        </span>
+        . I am also familiar with TypeScript and Prisma. I am always looking to
+        learn new technologies. I am currently looking for a
+        {' '}
+        <span className="font-medium">full-time position</span>
+        {' '}
+        as a front-end
+        developer.
+      </p>
+    </motion.section>
+  );
+}
