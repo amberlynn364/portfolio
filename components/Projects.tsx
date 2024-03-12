bb4f56d5a48218666ba971c9050e555614803940
amberlynn <amberlynn364@gmail.com>
2024-03-12 15:43:57 +0300

    feat: add animation for transitioning the active item in the header when clicking and scrolling on navigation

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,12 +3,15 @@
 import { projectsData } from '@/lib/data';
 import { Fragment } from 'react';
 import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
 import SectionHeading from './SectionHeading';
 import Project from './Project';
 
 export default function Projects() {
+  const { ref } = useSectionInView('Projects', 0.5);
+
   return (
-    <section id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="scroll-mt-28">
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
