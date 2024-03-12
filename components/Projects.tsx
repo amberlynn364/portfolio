181b652720365dd2221ca2b417b216ebf025b714
amberlynn <amberlynn364@gmail.com>
2024-03-12 22:42:30 +0300

    refactor: delete animation from heading and add margin to bottom section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,7 +2,6 @@
 
 import { projectsData } from '@/lib/data';
 import { Fragment } from 'react';
-import { motion } from 'framer-motion';
 import { useSectionInView } from '@/lib/hooks';
 import SectionHeading from './SectionHeading';
 import Project from './Project';
@@ -11,14 +10,8 @@ export default function Projects() {
   const { ref } = useSectionInView('Projects', 0.5);
 
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28">
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.2 }}
-      >
-        <SectionHeading>My projects</SectionHeading>
-      </motion.div>
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <Fragment key={index}>
