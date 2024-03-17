abbc61bf820d6b9ca6dd69f79d5f1b673cccbd76
amberlynn <amberlynn364@gmail.com>
2024-03-17 15:58:06 +0300

    style: update font-size at intro section

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -49,7 +49,7 @@ export default function Intro() {
         </div>
       </div>
       <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-[2.15rem]"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
